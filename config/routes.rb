ab0d87991207c226f41e2bc6a19595f54f744668
masashi <tmasara33@gmail.com>
2017-11-26 14:34:17 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -9,4 +7,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
