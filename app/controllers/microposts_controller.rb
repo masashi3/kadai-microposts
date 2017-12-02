6e4f51e69d5bfda36b54a4c3255b7d0d27658b57
masashi <tmasara33@gmail.com>
2017-12-02 15:04:42 +0000

    delete modify

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destory]
+  before_action :correct_user, only: [:destroy]
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -15,6 +15,7 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    current_user.unfavor(@micropost) if current_user.favorite?(@micropost)
     @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
@@ -27,7 +28,7 @@ class MicropostsController < ApplicationController
   end
   
   def correct_user
-    @micropost = current_user.microposts.find_bu(id: params[:id])
+    @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
       redirect_to root_url
     end
